edaa3a3b831714087705b1584ab31e8e96ad0058
Himilo4891 <118472464+Himilo4891@users.noreply.github.com>
2023-05-02 11:16:16 +0300

    Add files via upload

---
+++ Virtual Tourist 12/Virtual Tourist 12/CollectionViewCell.swift
@@ -0,0 +1,9 @@
+import Foundation
+import UIKit
+import Photos
+
+final class PhotoViewCell: UICollectionViewCell {
+    @IBOutlet weak var imagephoto: UIImageView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+}
