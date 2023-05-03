e143a92cf0356d74b503f12586f129f358c949bb
Himilo4891 <118472464+Himilo4891@users.noreply.github.com>
2023-05-03 10:39:20 +0300

    Delete Virtual Tourist 12 directory

--- Virtual Tourist 12/Virtual Tourist 12/CollectionViewCell.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-import UIKit
-import Photos
-
-final class PhotoViewCell: UICollectionViewCell {
-    @IBOutlet weak var imagephoto: UIImageView!
-    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
-    
-}
